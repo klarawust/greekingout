e737d4415a8532887c86f470afa36b95c6455fac
Klara Wüstenberg <klarawust@gmail.com>
2023-10-02 13:49:49 +0300

    make spacing in sidebar bigger

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -6,7 +6,7 @@ interface OverviewProps {
 
 export const Overview = ({ posts }: OverviewProps) => {
   return (
-    <ul className="flex md:flex-col gap-2 mt-12 flex-row">
+    <ul className="flex md:flex-col gap-4 mt-12 flex-row">
       <p className="text-amber-500 hover:scale-105">
         <a href="/about">About us</a>
       </p>
